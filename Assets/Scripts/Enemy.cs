a12206c5abbc9a184f300add5e4c6ee9e5431a63
nghiatran98 <nghiatran19980613@gmail.com>
2022-06-16 21:59:43 +0700

    Update ver 0.3
    - Upgrade player movement
    - Upgrade enemy movement
    - Add Simple Map

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    float _speed = 10.0f;
     [SerializeField] Transform playerPostion;
+    Rigidbody2D _enemyRb;
 
+    float _speed = 10.0f;
+    bool _isCollideWithWall = false;
+    bool _isCollideWithTopWall = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _enemyRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ChasePlayer();
+        EnemyMovement();
     }
 
-    void ChasePlayer()
+    void EnemyMovement()
     {
-        Vector2 distance = playerPostion.position - transform.position;
-        transform.Translate(distance.normalized * _speed * Time.deltaTime);
+        if (!_isCollideWithWall && !_isCollideWithTopWall)
+        {
+            Vector3 distance = playerPostion.position - transform.position;
+            _enemyRb.MovePosition(transform.position + distance.normalized * _speed * Time.deltaTime);
+        }
+
+        else
+        {
+            if (_isCollideWithTopWall)
+            {
+                _enemyRb.MovePosition(transform.position + Vector3.down * _speed * Time.deltaTime);
+            }
+            
+            else
+            {
+                _enemyRb.MovePosition(transform.position + Vector3.up * _speed * Time.deltaTime);
+            }
+
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -32,6 +52,36 @@ public class Enemy : MonoBehaviour
             Debug.Log("Game Over");
             Destroy(collision.gameObject);
             Destroy(gameObject);
+        }
+
+        else if (collision.gameObject.tag == "Wall")
+        {
+            _isCollideWithWall=true;
+            _enemyRb.MovePosition(transform.position + Vector3.up * Time.deltaTime);
+            Debug.Log("Collide with wall");
+        }
+
+        else if (collision.gameObject.tag == "TopWall")
+        {
+            _isCollideWithTopWall = true;
+            _enemyRb.MovePosition(transform.position + Vector3.down * Time.deltaTime);
+            Debug.Log("Collide with top wall");
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            _isCollideWithWall = false;
+            Debug.Log("Not collide with wall");
+        }
+
+        else if (collision.gameObject.tag == "TopWall") 
+        {
+            _isCollideWithTopWall = false;
+            Debug.Log("Not collide with top wall");
         }
     }
 }
